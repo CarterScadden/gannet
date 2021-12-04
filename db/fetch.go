1f6a5681cb6c3406870728be1b916b268761a1c7
Carter_E_Scadden <carter@srtlabs.com>
2021-12-04 10:57:11 -0700

    added fetch along with a test

--- db/fetch.go
+++ db/fetch.go
@@ -3,6 +3,18 @@ package db
 // Fetch
 // global database function which will grab a ProduceItem from the db.store by the given
 // produceCode
-func Fetch(produceCode string) {
-	// TODO: Fetch
+func Fetch(produceCodes ...string) []ProduceItem {
+	// TODO: potentially a better solution here
+	items := []ProduceItem{}
+
+	for _, code := range produceCodes {
+		for _, item := range store {
+			if code == item.ProduceCode {
+				items = append(items, item)
+				break
+			}
+		}
+	}
+
+	return items
 }
---
+++ db/fetch_test.go
@@ -0,0 +1,70 @@
+package db
+
+import "testing"
+
+func TestFetch(t *testing.T) {
+	// TODO: move this array, and the fetchTestProduceItemsEqual to some test utils file
+	items := []ProduceItem{
+		{
+			ProduceCode: "fetch-1",
+			Name:        "",
+			UnitPrice:   0.0,
+		}, {
+			ProduceCode: "fetch-2",
+			Name:        "",
+			UnitPrice:   0.0,
+		}, {
+			ProduceCode: "fetch-3",
+			Name:        "",
+			UnitPrice:   0.0,
+		}, {
+			ProduceCode: "fetch-4",
+			Name:        "",
+			UnitPrice:   0.0,
+		},
+	}
+
+	store = append(store, items...)
+
+	for _, item := range store {
+		fetched := Fetch(item.ProduceCode)
+
+		length := len(fetched)
+		if length != 1 {
+			t.Fatalf("number of fetched items for \"Fetch(item.ProduceCode)\" is not 1, got: %d\n", length)
+		}
+
+		if !fetchTestProduceItemsEqual(fetched[0], item) {
+			t.Fatalf("fetched item: %v != %v\n", fetched, item)
+		}
+	}
+
+	codes := []string{}
+
+	for _, item := range store {
+		codes = append(codes, item.ProduceCode)
+	}
+
+	codesToFetch := codes[1:]
+	fetched := Fetch(codesToFetch...)
+
+	for _, code := range codesToFetch {
+		found := false
+		for _, item := range fetched {
+			if code == item.ProduceCode {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			t.Fatalf("Failed to fetch code %s\n", code)
+		}
+	}
+}
+
+func fetchTestProduceItemsEqual(a, b ProduceItem) bool {
+	return a.Name == b.Name &&
+		a.UnitPrice == b.UnitPrice &&
+		a.ProduceCode == b.ProduceCode
+}
