38d1533e45df86b9da55e3a07da1bf7508ced22d
Carter_E_Scadden <carter@srtlabs.com>
2021-12-04 16:47:10 -0700

    added produce sample tests

--- db/produce/produce_test.go
+++ db/produce/produce_test.go
@@ -1,3 +1,40 @@
 package produce
 
+import "testing"
+
 // TODO: test that the produce functions work
+
+func TestValidateName(t *testing.T) {
+	shouldWork := []string{
+		"Apples",
+		"apples",
+		"apples    ",
+		"orange 2",
+		"9 awesome people are awesome",
+		"do not fail me now",
+	}
+
+	shouldNotWork := []string{
+		"  Apples ",
+		" Apples",
+		"! greenbeans",
+		"Lettuce#",
+	}
+
+	for _, should := range shouldWork {
+		if err := ValidateName(should); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for _, shouldNot := range shouldNotWork {
+		if err := ValidateName(shouldNot); err != nil {
+			t.Fatalf("`%s` marked as valid, when it should not be", shouldNot)
+		}
+	}
+
+	if err := ValidateName("Apples"); err == nil {
+		t.Fatal(err)
+	}
+
+}
